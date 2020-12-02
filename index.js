20be26ecb638c25a214786e0326e15c84ecf75fc
Sarthak <sarthak242@gmail.com>
2020-12-02 11:06:54 +0530

    feat: done with transaltion

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hi")
+var userInput = document.querySelector("#user-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var translatedOutput = document.querySelector("#translated-output");
+var apiUrl = "https://api.funtranslations.com/translate/minion.json"
+// var apiUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+btnTranslate.addEventListener("click", translateToBananaLang);
+
+function translateToBananaLang() {
+    let userInputText = userInput.value;
+    if ( userInputText ) {
+        fetch( getUrl( userInputText ) )
+        .then( response => response.json() )
+        .then( response => {
+            // translatedOutput.innerText = response.contents.text;
+            translatedOutput.innerText = response.contents.translated;
+        })
+    }
+}
+
+function getUrl(text) {
+    return apiUrl + '?' + 'text=' + text;
+}
